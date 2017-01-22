e0a64e7f99a8fe9dd565b3e6ac284f0272827d1e
neona <Passcode1>
2017-01-22 12:31:38 -0600

    add autocomplete to git, add definitions to memory autocomplete

---
+++ ScreepsAutocomplete/Memory.js
@@ -0,0 +1,13 @@
+/**
+ * Memory - Used to store arbitrary memory entries in arbitrary keys.
+ * @type {Object<string, any>}
+ */
+Memory: Object  = {
+    tasks: {},
+    subtasks: {},
+    reservations: {},
+    creeps: {},
+    spawns: {},
+    flags: {},
+    structures: {},
+  };
